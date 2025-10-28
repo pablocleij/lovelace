1cf31ababf5cc69c43f8c5a4a70d55dec537b116
Pablo Cleij <pablocley@hotmail.com>
2025-10-28 23:15:10 +0100

    Add writeEvent helper function for signed event sourcing

--- api.php
+++ api.php
@@ -2,5 +2,16 @@
 header('Content-Type: application/json');
 $apiConfig = json_decode(file_get_contents('cms/config/api_key.json'), true);
 $apiKey = $apiConfig['key'];
+
+function writeEvent($event){
+  $prevHash = ''; // load previous hash if exists
+  $event['previous_hash']=$prevHash;
+  $event['hash']=hash('sha256', json_encode($event));
+  $key = sodium_crypto_sign_keypair();
+  $event['signature']=base64_encode(sodium_crypto_sign_detached(json_encode($event), $key));
+  $id = str_pad(count(glob('cms/events/*.json'))+1,7,'0',STR_PAD_LEFT);
+  file_put_contents("cms/events/$id.json", json_encode($event,JSON_PRETTY_PRINT));
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 echo json_encode(['message'=>"Echo: ".$data['message']]);
