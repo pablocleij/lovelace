83ce850b4a667ba59cfa818789aba8e99b85f4ed
Pablo Cleij <pablocley@hotmail.com>
2025-10-28 23:00:19 +0100

    Initial commit: lovelace CMS structure
    - Created folder structure for file-based CMS
    - Added core PHP files (index, api, replay, preview)
    - Set up cms directories for collections, events, snapshots, etc.
    - Added minimal HTML chat interface

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>lovelace</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div id="chat-container"></div>
+  <script src="js/chat.js" type="module"></script>
+</body>
+</html>
