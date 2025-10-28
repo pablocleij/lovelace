deb39e7180ad505d753d8d856bf3de573a0b9bc6
Pablo Cleij <pablocley@hotmail.com>
2025-10-28 23:14:23 +0100

    Add BYOK (Bring Your Own Key) support with API config

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
 header('Content-Type: application/json');
+$apiConfig = json_decode(file_get_contents('cms/config/api_key.json'), true);
+$apiKey = $apiConfig['key'];
 $data = json_decode(file_get_contents('php://input'), true);
 echo json_encode(['message'=>"Echo: ".$data['message']]);
